49711dec971efaecb5a00cc7bf511a28ee0e15cd
Stefan Cretu <stefan.cretu@optimaize.com>
2017-08-27 03:14:46 +0300

    New risk detector API: FakeRiskType enum: renamed misspelled HUMOROUS value, improved Javadoc.

--- core/src/main/java/org/nameapi/ontology5/services/riskdetector/FakeRiskType.java
+++ core/src/main/java/org/nameapi/ontology5/services/riskdetector/FakeRiskType.java
@@ -1,70 +1,89 @@
 package org.nameapi.ontology5.services.riskdetector;
 
 /**
- * Lists all the possible types of risks.
+ * Classification of risks.
+ *
+ * <p>In some situations the exact classification is difficult.
+ * For example a person's name may be from fiction, but also be famous at the same time.</p>
  */
 public enum FakeRiskType implements RiskType {
-    
+
     /**
-     * Such as "asdf asdf".
+     * Example: "asdf asdf".
+     *
+     * This kind of input is often used to quickly pass mandatory fields in a form.
      */
     RANDOM_TYPING,
 
     /**
      * Examples:
-     * 
-     * For name: "John Doe".
-     * 
-     * For title: The given name field doesn't contain a given name, but has at least a title.
+     *
+     * For person name: "John Doe".
+     *
+     * For person title: Example: "King Peter"
+     *            The given name field doesn't contain a given name, but has at least a title.
      *            It may, in addition, contain a salutation.
-     *            Example: "King Peter"
-     * 
-     * For salutation: The given name field doesn't contain a given name, but has a salutation.
-     * There is no title in it, otherwise PLACEHOLDER_TITLE would be used.
-     * Example: "Mr. Smith" (Mr. in the given name field).
+     *
+     * For salutation: Example: "Mr. Smith" (Mr. in the given name field).
+     *                 The given name field doesn't contain a given name, but has a salutation.
+     *                 There is no title in it, otherwise PLACEHOLDER_TITLE would be used.
+     *
+     * For place name: "Anytown"
      */
     PLACEHOLDER,
-    
+
     /**
      * Examples:
-     * 
-     * For name: "James Bond".
-     * For Organization: ACME (American Company Making Everything)
-     * 
-     * For place: Atlantis, Entenhausen
+     *
+     * For natural person: "James Bond".
+     * For legal person: ACME (American Company Making Everything)
+     *
+     * For place: "Atlantis", "Entenhausen"
      */
     FICTIONAL,
 
     /**
-     * Name example: "Barak Obama".
-     * 
-     * Address example: the address of the white house.
+     * Examples:
+     *
+     * For natural person: "Barak Obama".
      */
     FAMOUS,
 
     /**
-     * Name example: "Sandy Beach".
-     * Place example: Fucking (in Austria)
+     * Examples:
+     *
+     * For natural person: "Sandy Beach".
+     * Place example: "Timbuckthree"
      */
-    HUMEROUS,
+    HUMOROUS,
 
     /**
-     * Examples for name: 
-     * "firstname lastname"
-     * 
+     * This includes multiple types of invalid form input.
+     *
+     * Refusing input:
+     * Example: "None of your business"
+     *
+     * Placeholder nouns: "Someone", "Somebody else", "Somewhere", "Nowhere"
+     *
+     * Repeating the form fields:
+     * Example for person name: "firstname lastname"
      * Examples for street: "Street"
-     * 
-     * Examples in general: such as vulgar language "fuck off", etc.
+     *
+     * Vulgar language, swearing
+     * Examples: "fuck off"
      */
     INVALID,
 
     /**
-     * The gn and sn field are similar: equal, levenshtein-similar, starts-with, ends-with.
+     * The given name and surname field are equal or almost equal, or match a certain pattern.
+     * Example: "John" / "John"
+     *
+     * The risk score is culture adjusted. In some cultures such names do exist, however, a risk is still raised.
      */
     STRING_SIMILARITY,
 
     /**
-     * Everything not in any of the other categories (enum values).
+     * Everything that does not fit into any of the other categories.
      */
     OTHER,
     ;
